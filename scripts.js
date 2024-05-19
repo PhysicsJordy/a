83bd9cc25d6c864279180f1e0665b4cb0f74a90b
PhysicsJordy <169025498+PhysicsJordy@users.noreply.github.com>
2024-05-19 13:18:34 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1759,17 +1759,18 @@ document.getElementById('calculator-form').addEventListener('submit', function (
         const { materials, additionalMoney } = calculateBreakthroughResources(startLevel, targetLevel);
         const totalMoneyIncludingBreakthrough = totalMoney + additionalMoney;
 
-        document.getElementById('result').innerHTML = `
-            <p>필요한 연마석 개수: ${totalStones}</p>
-            <p>연마에만 필요한 금액: ${formatNumber(totalMoney)}</p>
-            <p>돌파에만 필요한 금액: ${formatNumber(additionalMoney)}</p>
-            <p>총 금액 (돌파+연마): ${formatNumber(totalMoneyIncludingBreakthrough)}</p>
-            <p>필요한 돌파 재료:</p>
-              <ul>${materials.map(mat => {
-              const parts = mat.split(' x ');
-              return `<li>${parts[1]}
-            }).join('')}</ul>
-        `;
+document.getElementById('result').innerHTML = `
+    <p>필요한 연마석 개수: ${totalStones}</p>
+    <p>연마에만 필요한 금액: ${formatNumber(totalMoney)}</p>
+    <p>돌파에만 필요한 금액: ${formatNumber(additionalMoney)}</p>
+    <p>총 금액 (돌파+연마): ${formatNumber(totalMoneyIncludingBreakthrough)}</p>
+    <p>필요한 돌파 재료:</p>
+    <ul>${materials.map(mat => {
+        const parts = mat.split(' X ');
+        return `<li>${parts[0]}</li>`;
+    }).join('')}</ul>
+`;
+
     } else {
         document.getElementById('result').innerHTML = `
             <p>올바른 레벨 범위를 입력해주세요 (시작 레벨은 목표 레벨보다 낮아야 합니다.).</p>
